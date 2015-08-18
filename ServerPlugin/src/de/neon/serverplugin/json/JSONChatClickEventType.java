43fe100be509880a8e9d056bda4380a88cf6ee6e
Speidy674 <Marvin@Marvin-PC.fritz.box>
2015-08-18 23:13:44 +0200

    :)

--- ServerPlugin/src/de/neon/serverplugin/json/JSONChatClickEventType.java
+++ ServerPlugin/src/de/neon/serverplugin/json/JSONChatClickEventType.java
@@ -3,7 +3,6 @@ package de.neon.serverplugin.json;
 public enum JSONChatClickEventType {
     RUN_COMMAND("run_command"),
     SUGGEST_COMMAND("suggest_command"),
-    ASDSHGUUSDIGF("fsdgf"),
     OPEN_URL("open_url");
     private final String type;
 
