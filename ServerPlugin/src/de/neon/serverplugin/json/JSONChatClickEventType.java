52fe5c21e738d02538fded48585535f4a239032a
Simon <Simon@192.168.2.141>
2015-08-18 23:10:53 +0200

    dfsdf

--- ServerPlugin/src/de/neon/serverplugin/json/JSONChatClickEventType.java
+++ ServerPlugin/src/de/neon/serverplugin/json/JSONChatClickEventType.java
@@ -3,6 +3,7 @@ package de.neon.serverplugin.json;
 public enum JSONChatClickEventType {
     RUN_COMMAND("run_command"),
     SUGGEST_COMMAND("suggest_command"),
+    ASDSHGUUSDIGF("fsdgf"),
     OPEN_URL("open_url");
     private final String type;
 
