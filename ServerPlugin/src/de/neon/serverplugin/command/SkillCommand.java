7c6abaf82a48c67961b90fddb77f29ae336e6ac2
Simon <Simon@192.168.2.141>
2015-08-19 03:34:36 +0200

    dfssdfsdf

--- ServerPlugin/src/de/neon/serverplugin/command/SkillCommand.java
+++ ServerPlugin/src/de/neon/serverplugin/command/SkillCommand.java
@@ -1,17 +1,17 @@
 package de.neon.serverplugin.command;
 
-import net.minecraft.server.v1_8_R3.IChatBaseComponent.ChatSerializer;
-import net.minecraft.server.v1_8_R3.Item;
-import net.minecraft.server.v1_8_R3.ItemStack;
-import net.minecraft.server.v1_8_R3.NBTTagCompound;
-import net.minecraft.server.v1_8_R3.NBTTagString;
+import java.util.List;
 
-import org.bukkit.ChatColor;
+import net.minecraft.server.v1_8_R3.IChatBaseComponent;
+import net.minecraft.server.v1_8_R3.IChatBaseComponent.ChatSerializer;
+
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.craftbukkit.v1_8_R3.inventory.CraftItemStack;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BookMeta;
 
 import de.neon.serverplugin.DataUtil;
 
@@ -24,7 +24,16 @@ public class SkillCommand implements CommandExecutor {
 			if(cmd.getName().equalsIgnoreCase("skill")) {
 				try {
 					if(DataUtil.geti(p, "skillpoints") > 0) {
-						p.getInventory().addItem(getBook(p));
+						ItemStack book = new ItemStack(Material.WRITTEN_BOOK);
+						BookMeta meta = (BookMeta) book.getItemMeta();
+						try {
+						     java.lang.reflect.Field pagesField = Class.forName("org.bukkit.craftbukkit.v1_8_R3.inventory.CraftMetaBook").getDeclaredField("pages");
+						     pagesField.setAccessible(true);
+						     List<IChatBaseComponent> pages = (List<IChatBaseComponent>) pagesField.get(meta);
+						     pages.add(ChatSerializer.a("{pages:[,{\\\"text\\\":\\\"Stats\n\\\",\\\"bold\\\":true,\\\"underlined\\\":true},{\\\"text\\\":\\\"\nStärke:\\\",\\\"color\\\":\\\"aqua\\\",\\\"bold\\\":false,\\\"underlined\\\":false},{\\\"text\\\":\\\""+DataUtil.geti(p, "strength")+"\\\",\\\"color\\\":\\\"gold\\\"},{\\\"text\\\":\\\"[+]\\\",\\\"color\\\":\\\"red\\\",\\\"bold\\\":true,\\\"clickEvent\\\":{\\\"action\\\":\\\"run_command\\\",\\\"value\\\":\\\"/stats strength\\\"},\\\"hoverEvent\\\":{\\\"action\\\":\\\"show_text\\\",\\\"value\\\":{\\\"text\\\":\\\"\\\",\\\"extra\\\":[{\\\"text\\\":\\\"Erhöhe Stärke\\\"}]}}},{\\\"text\\\":\\\"\nVerteidigung:\\\",\\\"color\\\":\\\"aqua\\\",\\\"bold\\\":false},{\\\"text\\\":\\\""+DataUtil.geti(p, "defensive")+"\\\",\\\"color\\\":\\\"gold\\\"},{\\\"text\\\":\\\"[+]\\\",\\\"color\\\":\\\"red\\\",\\\"bold\\\":true,\\\"clickEvent\\\":{\\\"action\\\":\\\"run_command\\\",\\\"value\\\":\\\"/stats defense\\\"},\\\"hoverEvent\\\":{\\\"action\\\":\\\"show_text\\\",\\\"value\\\":{\\\"text\\\":\\\"\\\",\\\"extra\\\":[{\\\"text\\\":\\\"Erhöhe deine Verteidigung\\\"}]}}},{\\\"text\\\":\\\"\nVitalität:\\\",\\\"color\\\":\\\"aqua\\\",\\\"bold\\\":false},{\\\"text\\\":\\\""+DataUtil.geti(p, "vitality")+"\\\",\\\"color\\\":\\\"gold\\\"},{\\\"text\\\":\\\"[+]\\\",\\\"color\\\":\\\"red\\\",\\\"bold\\\":true,\\\"clickEvent\\\":{\\\"action\\\":\\\"run_command\\\",\\\"value\\\":\\\"/stats vitality\\\"},\\\"hoverEvent\\\":{\\\"action\\\":\\\"show_text\\\",\\\"value\\\":{\\\"text\\\":\\\"\\\",\\\"extra\\\":[{\\\"text\\\":\\\"Erhöhe deine Vitalität\\\"}]}}},{\\\"text\\\":\\\"\nGeschicklichkeit:\\\",\\\"color\\\":\\\"aqua\\\",\\\"bold\\\":false},{\\\"text\\\":\\\""+DataUtil.geti(p, "dexterity")+"\\\",\\\"color\\\":\\\"gold\\\"},{\\\"text\\\":\\\"[+]\\\",\\\"color\\\":\\\"red\\\",\\\"bold\\\":true,\\\"clickEvent\\\":{\\\"action\\\":\\\"run_command\\\",\\\"value\\\":\\\"/stats dexterity\\\"},\\\"hoverEvent\\\":{\\\"action\\\":\\\"show_text\\\",\\\"value\\\":{\\\"text\\\":\\\"\\\",\\\"extra\\\":[{\\\"text\\\":\\\"Erhöhe deine Geschicklichkeit\\\"}]}}}}"));
+						} catch(Exception e) {
+							e.printStackTrace();
+						}
 					} else {
 						
 					}
@@ -35,19 +44,5 @@ public class SkillCommand implements CommandExecutor {
 			}
 		}
 		return false;
-	}
-
-	private CraftItemStack getBook(Player p) {
-		ItemStack stack = new ItemStack(Item.d("written_book"));
-		NBTTagCompound tag = new NBTTagCompound();
-		if(!stack.hasTag()) {
-			tag = new NBTTagCompound();
-			stack.setTag(tag);
-		}
-		tag.set("author", new NBTTagString(ChatColor.MAGIC+"aaaaa"));
-		tag.set("title", new NBTTagString(ChatColor.GOLD+"Skill-Buch"));
-		tag.set("pages", new NBTTagString("{\"text\":\"Stats\n\",\"bold\":true,\"underlined\":true},{\"text\":\"\nStärke:\",\"color\":\"aqua\",\"bold\":false,\"underlined\":false},{\"text\":\""+DataUtil.geti(p, "strength")+"\",\"color\":\"gold\"},{\"text\":\"[+]\",\"color\":\"red\",\"bold\":true,\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/stats strength\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":{\"text\":\"\",\"extra\":[{\"text\":\"Erhöhe Stärke\"}]}}},{\"text\":\"\nVerteidigung:\",\"color\":\"aqua\",\"bold\":false},{\"text\":\""+DataUtil.geti(p, "defensive")+"\",\"color\":\"gold\"},{\"text\":\"[+]\",\"color\":\"red\",\"bold\":true,\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/stats defense\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":{\"text\":\"\",\"extra\":[{\"text\":\"Erhöhe deine Verteidigung\"}]}}},{\"text\":\"\nVitalität:\",\"color\":\"aqua\",\"bold\":false},{\"text\":\""+DataUtil.geti(p, "vitality")+"\",\"color\":\"gold\"},{\"text\":\"[+]\",\"color\":\"red\",\"bold\":true,\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/stats vitality\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":{\"text\":\"\",\"extra\":[{\"text\":\"Erhöhe deine Vitalität\"}]}}},{\"text\":\"\nGeschicklichkeit:\",\"color\":\"aqua\",\"bold\":false},{\"text\":\""+DataUtil.geti(p, "dexterity")+"\",\"color\":\"gold\"},{\"text\":\"[+]\",\"color\":\"red\",\"bold\":true,\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/stats dexterity\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":{\"text\":\"\",\"extra\":[{\"text\":\"Erhöhe deine Geschicklichkeit\"}]}}}"));
-		stack.setTag(tag);
-		return CraftItemStack.asCraftMirror(stack);
 	}
 }
