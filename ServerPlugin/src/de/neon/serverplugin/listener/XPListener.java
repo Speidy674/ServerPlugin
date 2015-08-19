5973e0d48e684f022af3407f909b371375a82bdf
Speidy674 <Marvin@Marvin-PC.fritz.box>
2015-08-19 04:07:22 +0200

    XP Anzeige

--- ServerPlugin/src/de/neon/serverplugin/listener/XPListener.java
+++ ServerPlugin/src/de/neon/serverplugin/listener/XPListener.java
@@ -1,8 +1,12 @@
 package de.neon.serverplugin.listener;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Color;
 import org.bukkit.FireworkEffect;
+import org.bukkit.Location;
 import org.bukkit.entity.Firework;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
@@ -14,6 +18,7 @@ import org.bukkit.inventory.meta.FireworkMeta;
 import de.neon.serverplugin.DataUtil;
 import de.neon.serverplugin.Util;
 import de.neon.serverplugin.event.PlayerLevelUpEvent;
+import de.neon.serverplugin.hologram.Hologram;
 
 public class XPListener implements Listener {
 
@@ -25,6 +30,20 @@ public class XPListener implements Listener {
 			Player p = (Player) e2;
 			if(DataUtil.geti(p, "level") >= 60) return;
 			DataUtil.increasef(p, "xp", e.getDroppedExp() / 3.5f);
+			
+			List<String> lines = new ArrayList<String>();
+            lines.add("§7[§aXP§7]");
+            lines.add("§4+"+Util.formatFloat(e.getDroppedExp() / 3.5f, 1));
+		
+            final Location loc = e1.getLocation();
+            loc.setY(e2.getLocation().getY()-1);
+            
+            Hologram hologram = new Hologram();
+            hologram.setLines(lines);
+            hologram.setLocation(loc);
+            hologram.setHeight(1.5D);
+            hologram.spawntemp(60);
+			
 			if(DataUtil.getf(p, "xp") >= Util.experienceRequied(DataUtil.geti(p, "level")+1)) {
 				DataUtil.increasei(p, "level", 1);
 				DataUtil.increasei(p, "skillpoints", 1);
