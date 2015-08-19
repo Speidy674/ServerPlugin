50378350a6bcafb8115e65df9e5e3122fd8c1f13
Speidy674 <Marvin@Marvin-PC.fritz.box>
2015-08-19 02:06:37 +0200

    D:awdawdawd

--- ServerPlugin/src/de/neon/serverplugin/listener/JoinLeaveListener.java
+++ ServerPlugin/src/de/neon/serverplugin/listener/JoinLeaveListener.java
@@ -9,13 +9,13 @@ public class JoinLeaveListener implements Listener {
 
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent e) {
-		String msg = "§7[§a+§7]§6"+e.getPlayer().getName();
+		String msg = "§7[§a+§7] §6"+e.getPlayer().getName();
 		e.setJoinMessage(msg);
 	}
 	
 	@EventHandler
 	public void onPlayerLeave(PlayerQuitEvent e) {
-		String msg = "§7[§c-§7]§6"+e.getPlayer().getName();
+		String msg = "§7[§c-§7] §6"+e.getPlayer().getName();
 		e.setQuitMessage(msg);
 		
 	}
