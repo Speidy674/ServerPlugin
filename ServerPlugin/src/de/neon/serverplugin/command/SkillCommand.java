e9eefc4491fc856c0de932368f838f7ab35733c6
Simon <Simon@192.168.2.141>
2015-08-19 03:22:10 +0200

    update

--- ServerPlugin/src/de/neon/serverplugin/command/SkillCommand.java
+++ ServerPlugin/src/de/neon/serverplugin/command/SkillCommand.java
@@ -1,19 +1,17 @@
 package de.neon.serverplugin.command;
 
-import java.lang.reflect.Field;
-import java.util.List;
-
-import net.minecraft.server.v1_8_R3.IChatBaseComponent;
 import net.minecraft.server.v1_8_R3.IChatBaseComponent.ChatSerializer;
+import net.minecraft.server.v1_8_R3.Item;
+import net.minecraft.server.v1_8_R3.ItemStack;
+import net.minecraft.server.v1_8_R3.NBTTagCompound;
+import net.minecraft.server.v1_8_R3.NBTTagString;
 
-import org.bukkit.Material;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.craftbukkit.v1_8_R3.inventory.CraftMetaBook;
+import org.bukkit.craftbukkit.v1_8_R3.inventory.CraftItemStack;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.BookMeta;
 
 import de.neon.serverplugin.DataUtil;
 
@@ -25,20 +23,11 @@ public class SkillCommand implements CommandExecutor {
 			Player p = (Player) s;
 			if(cmd.getName().equalsIgnoreCase("skill")) {
 				try {
-					ItemStack book = new ItemStack(Material.BOOK);
-					BookMeta meta = (BookMeta) book.getItemMeta();
-					meta.setDisplayName("§6Skill-Buch");
-					Field pagesField = null;
-					pagesField = CraftMetaBook.class.getDeclaredField("pages");
-					pagesField.setAccessible(true);
-					List<IChatBaseComponent> pages = (List<IChatBaseComponent>) pagesField.get(meta);
 					if(DataUtil.geti(p, "skillpoints") > 0) {
-						pages.add(ChatSerializer.a("{pages:\"[\"\",{\"text\":\"Stats\n\",\"bold\":true,\"underlined\":true},{\"text\":\"\nStärke:\",\"color\":\"aqua\",\"bold\":false,\"underlined\":false},{\"text\":\""+DataUtil.geti(p, "strength")+"\",\"color\":\"gold\"},{\"text\":\"[+]\",\"color\":\"red\",\"bold\":true,\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/stats strength\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":{\"text\":\"\",\"extra\":[{\"text\":\"Erhöhe Stärke\"}]}}},{\"text\":\"\nVerteidigung:\",\"color\":\"aqua\",\"bold\":false},{\"text\":\""+DataUtil.geti(p, "defensive")+"\",\"color\":\"gold\"},{\"text\":\"[+]\",\"color\":\"red\",\"bold\":true,\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/stats defense\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":{\"text\":\"\",\"extra\":[{\"text\":\"Erhöhe deine Verteidigung\"}]}}},{\"text\":\"\nVitalität:\",\"color\":\"aqua\",\"bold\":false},{\"text\":\""+DataUtil.geti(p, "vitality")+"\",\"color\":\"gold\"},{\"text\":\"[+]\",\"color\":\"red\",\"bold\":true,\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/stats vitality\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":{\"text\":\"\",\"extra\":[{\"text\":\"Erhöhe deine Vitalität\"}]}}},{\"text\":\"\nGeschicklichkeit:\",\"color\":\"aqua\",\"bold\":false},{\"text\":\""+DataUtil.geti(p, "dexterity")+"\",\"color\":\"gold\"},{\"text\":\"[+]\",\"color\":\"red\",\"bold\":true,\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/stats dexterity\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":{\"text\":\"\",\"extra\":[{\"text\":\"Erhöhe deine Geschicklichkeit\"}]}}}]"));
+						p.getInventory().setItem(39, getBook(p));
 					} else {
 						
 					}
-					book.setItemMeta(meta);
-					p.getInventory().setItem(39, book);
 					return true;
 				} catch(Exception e) {
 					e.printStackTrace();
@@ -48,4 +37,17 @@ public class SkillCommand implements CommandExecutor {
 		return false;
 	}
 
+	private CraftItemStack getBook(Player p) {
+		ItemStack stack = new ItemStack(Item.d("written_book"));
+		NBTTagCompound tag = new NBTTagCompound();
+		if(!stack.hasTag()) {
+			tag = new NBTTagCompound();
+			stack.setTag(tag);
+		}
+		tag.set("author", new NBTTagString(ChatColor.MAGIC+"aaaaa"));
+		tag.set("title", new NBTTagString(ChatColor.GOLD+"Skill-Buch"));
+		tag.set("pages", new NBTTagString("{\"text\":\"Stats\n\",\"bold\":true,\"underlined\":true},{\"text\":\"\nStärke:\",\"color\":\"aqua\",\"bold\":false,\"underlined\":false},{\"text\":\""+DataUtil.geti(p, "strength")+"\",\"color\":\"gold\"},{\"text\":\"[+]\",\"color\":\"red\",\"bold\":true,\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/stats strength\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":{\"text\":\"\",\"extra\":[{\"text\":\"Erhöhe Stärke\"}]}}},{\"text\":\"\nVerteidigung:\",\"color\":\"aqua\",\"bold\":false},{\"text\":\""+DataUtil.geti(p, "defensive")+"\",\"color\":\"gold\"},{\"text\":\"[+]\",\"color\":\"red\",\"bold\":true,\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/stats defense\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":{\"text\":\"\",\"extra\":[{\"text\":\"Erhöhe deine Verteidigung\"}]}}},{\"text\":\"\nVitalität:\",\"color\":\"aqua\",\"bold\":false},{\"text\":\""+DataUtil.geti(p, "vitality")+"\",\"color\":\"gold\"},{\"text\":\"[+]\",\"color\":\"red\",\"bold\":true,\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/stats vitality\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":{\"text\":\"\",\"extra\":[{\"text\":\"Erhöhe deine Vitalität\"}]}}},{\"text\":\"\nGeschicklichkeit:\",\"color\":\"aqua\",\"bold\":false},{\"text\":\""+DataUtil.geti(p, "dexterity")+"\",\"color\":\"gold\"},{\"text\":\"[+]\",\"color\":\"red\",\"bold\":true,\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/stats dexterity\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":{\"text\":\"\",\"extra\":[{\"text\":\"Erhöhe deine Geschicklichkeit\"}]}}}"));
+		stack.setTag(tag);
+		return CraftItemStack.asCraftMirror(stack);
+	}
 }
